a753f2c26d0ac1a52d1586cd05104059f897f2f1
Andy Jarrett <andy@mytag.io>
2024-06-07 16:45:07 +0100

    change the folder structure

--- scripts/voices.sh
+++ scripts/voices.sh
@@ -9,20 +9,22 @@
 say -o startingin.aiff "Starting in 5.4.3.2.1"
 
 # Convert the AIFF file to MP3 using lame
-lame -m m startingin.aiff ../mp3s/startingin.mp3
+lame -m m startingin.aiff startingin.mp3
 
 # Generate the AIFF file for the beep sound
 say -v Bells -o beep.aiff "beep"
 
 # Convert the AIFF file to MP3 using lame
-lame -m m beep.aiff ../mp3s/beep.mp3
+lame -m m beep.aiff beep.mp3
 
 # Generate the AIFF file for the beep sound
 say -o finished.aiff "The beep test has finished. Well done!"
 
 # Convert the AIFF file to MP3 using lame
-lame -m m finished.aiff ../mp3s/finished.mp3
+lame -m m finished.aiff finished.mp3
 
 rm *.aiff
+
+mv *.mp3 ../src/mp3s/
 
 echo "Audio files created and conversion completed"
